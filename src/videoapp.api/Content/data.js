812ee7c158680b02cc7e848c93e62be70d038281
filip.vukovinski <filip.vukovinski@net.hr>
2021-08-02 06:50:12 +0200

    some work on web page

---
+++ src/videoapp.api/Content/data.js
@@ -0,0 +1,395 @@
+﻿const data = [
+  {
+    "retrievedAt": "2021-08-01T19:40:37.106358Z",
+    "imdbData": {
+      "movie": {
+        "id": "tt3778644",
+        "resultType": "Title",
+        "image": "https://imdb-api.com/images/original/MV5BOTM2NTI3NTc3Nl5BMl5BanBnXkFtZTgwNzM1OTQyNTM@._V1_Ratio0.7273_AL_.jpg",
+        "title": "Solo: A Star Wars Story",
+        "description": "(2018)"
+      },
+      "rating": {
+        "imDbId": "tt3778644",
+        "title": "Solo: A Star Wars Story",
+        "fullTitle": "Solo: A Star Wars Story (2018)",
+        "type": "Movie",
+        "year": "2018",
+        "imDb": "6.9",
+        "metacritic": "62",
+        "theMovieDb": "6.6",
+        "rottenTomatoes": "",
+        "tV_com": "",
+        "filmAffinity": "6.0",
+        "errorMessage": ""
+      }
+    },
+    "youtubeTrailersData": {
+      "kind": "youtube#searchListResponse",
+      "etag": "t7n_TYQPgjna8BwAz7t7uldQ3b8",
+      "nextPageToken": "CAUQAA",
+      "regionCode": "US",
+      "pageInfo": {
+        "totalResults": 429085,
+        "resultsPerPage": 5
+      },
+      "items": [
+        {
+          "kind": "youtube#searchResult",
+          "etag": "exebD8O5i0zd0KDMnfcPHo8oo0Y",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "7dvLdszjsDU"
+          },
+          "snippet": {
+            "publishedAt": "2018-02-04T23:53:29Z",
+            "channelId": "UC_vz6SvmIkYs1_H3Wv2SKlg",
+            "title": "Solo: A Star Wars Story (2018) | SUPER BOWL TRAILER",
+            "description": "During an adventure into a dark criminal underworld, Han Solo meets his future copilot Chewbacca and encounters Lando Calrissian years before joining the ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/7dvLdszjsDU/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/7dvLdszjsDU/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "IMDb",
+            "liveBroadcastContent": "none",
+            "publishTime": "2018-02-04T23:53:29Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "jJeRI24YNN9bt2jkl89YueOyDQQ",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "U3asfxRMRtE"
+          },
+          "snippet": {
+            "publishedAt": "2018-04-16T14:16:41Z",
+            "channelId": "UCzNWVDZQ55bjq8uILZ7_wyQ",
+            "title": "SOLO: A STAR WARS STORY &quot;Chewie Meets Han&quot; Trailer",
+            "description": "The Latest BLOCKBUSTERS are HERE ▻ https://www.youtube.com/playlist?list=PL3HDxuj_oceDEQGSpLLOqZjjy05MwFOSp SOLO: A STAR WARS STORY ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/U3asfxRMRtE/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/U3asfxRMRtE/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "FRESH Movie Trailers",
+            "liveBroadcastContent": "none",
+            "publishTime": "2018-04-16T14:16:41Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "eKSqHyejGnD46yk_rvB0jShLPyo",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "8N3Tqa4Cr20"
+          },
+          "snippet": {
+            "publishedAt": "2018-02-06T21:34:20Z",
+            "channelId": "UCCI2URl1uXl8ijfoboX9nRg",
+            "title": "New Trailer | Solo: A Star Wars Story (2018)",
+            "description": "During an adventure into a dark criminal underworld, Han Solo meets his future copilot Chewbacca and encounters Lando Calrissian years before joining the ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/8N3Tqa4Cr20/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/8N3Tqa4Cr20/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "247freehdmovie",
+            "liveBroadcastContent": "none",
+            "publishTime": "2018-02-06T21:34:20Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "TOyq4pUOdtpStsr82hkrvcVZKO8",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "UtxkUICY9wM"
+          },
+          "snippet": {
+            "publishedAt": "2018-04-09T04:21:03Z",
+            "channelId": "UCvnL1w1-5z0o7kGQy-v6XOw",
+            "title": "SOLO: A Star Wars Story Trailer 2 (2018)",
+            "description": "The second Trailer for \"Solo: A Star Wars Story\" Note | Solo: A Star Wars Story trailer courtesy of Disney. | All Rights Reserved.",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/UtxkUICY9wM/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/UtxkUICY9wM/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "KinoStar Trailer",
+            "liveBroadcastContent": "none",
+            "publishTime": "2018-04-09T04:21:03Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "zWBcJf87QypfLjcSU_VxzVzvmds",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "jfSFCdIVLWU"
+          },
+          "snippet": {
+            "publishedAt": "2018-04-09T07:05:48Z",
+            "channelId": "UCE-91a9iNIN9C7u_NgSCVyw",
+            "title": "SOLO: A Star Wars Story - Trailer - Star Wars NL",
+            "description": "Bekijk hier de nieuwe trailer van Solo: A Star Wars Story. Vanaf 23 mei in de bioscoop! Kijk voor meer info en tickets: http://disney.nl/films/solo-a-star-wars-story.",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/jfSFCdIVLWU/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/jfSFCdIVLWU/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "Star Wars NL",
+            "liveBroadcastContent": "none",
+            "publishTime": "2018-04-09T07:05:48Z"
+          }
+        }
+      ]
+    }
+  },
+  {
+    "retrievedAt": "2021-08-01T19:40:37.0750576Z",
+    "imdbData": {
+      "movie": {
+        "id": "tt3748528",
+        "resultType": "Title",
+        "image": "https://imdb-api.com/images/original/MV5BMjEwMzMxODIzOV5BMl5BanBnXkFtZTgwNzg3OTAzMDI@._V1_Ratio0.7273_AL_.jpg",
+        "title": "Rogue One: A Star Wars Story",
+        "description": "(2016)"
+      },
+      "rating": {
+        "imDbId": "tt3748528",
+        "title": "Rogue One: A Star Wars Story",
+        "fullTitle": "Rogue One: A Star Wars Story (2016)",
+        "type": "Movie",
+        "year": "2016",
+        "imDb": "7.8",
+        "metacritic": "65",
+        "theMovieDb": "7.5",
+        "rottenTomatoes": "",
+        "tV_com": "7.2",
+        "filmAffinity": "6.9",
+        "errorMessage": ""
+      }
+    },
+    "youtubeTrailersData": {
+      "kind": "youtube#searchListResponse",
+      "etag": "ia_Y8riYhlbhm31yoHQwUYJvaWw",
+      "nextPageToken": "CAUQAA",
+      "regionCode": "US",
+      "pageInfo": {
+        "totalResults": 84336,
+        "resultsPerPage": 5
+      },
+      "items": [
+        {
+          "kind": "youtube#searchResult",
+          "etag": "iT_kqVIgR4W_Mkh5i0zP7_PPpgY",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "uKDdMAI-onI"
+          },
+          "snippet": {
+            "publishedAt": "2016-04-07T13:49:40Z",
+            "channelId": "UCi8e0iOVk1fEOogdfu4YgfA",
+            "title": "Rogue One: A Star Wars Story Official Teaser Trailer #1 (2016) - Felicity Jones Movie HD",
+            "description": "Get Tickets - http://www.fandango.com/rogueone:astarwarsstory2016_187530/movieoverview?cmp=MCYT_YouTube_Desc Subscribe to TRAILERS: ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/uKDdMAI-onI/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/uKDdMAI-onI/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "Movieclips Trailers",
+            "liveBroadcastContent": "none",
+            "publishTime": "2016-04-07T13:49:40Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "DikT4FRxVsIncO-o4dhJfNDbJJY",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "Wji-BZ0oCwg"
+          },
+          "snippet": {
+            "publishedAt": "2016-04-07T11:49:27Z",
+            "channelId": "UCZGYJFUizSax-yElQaFDp5Q",
+            "title": "ROGUE ONE: A STAR WARS STORY Official Teaser Trailer",
+            "description": "Get your first look at ROGUE ONE: A STAR WARS STORY in theaters this December. Visit Star Wars at http://www.starwars.com Subscribe to Star Wars on ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/Wji-BZ0oCwg/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/Wji-BZ0oCwg/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "Star Wars",
+            "liveBroadcastContent": "none",
+            "publishTime": "2016-04-07T11:49:27Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "c7QblYGj20tHNGvHq2CldbLpC78",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "sC9abcLLQpI"
+          },
+          "snippet": {
+            "publishedAt": "2016-10-13T11:47:33Z",
+            "channelId": "UCZGYJFUizSax-yElQaFDp5Q",
+            "title": "Rogue One: A Star Wars Story Trailer #2 (Official)",
+            "description": "Watch the official second trailer for Rogue One: A Star Wars Story, in which Jyn Erso leads a group of unlikely heroes to steal the plans to the Death Star, the ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/sC9abcLLQpI/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/sC9abcLLQpI/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "Star Wars",
+            "liveBroadcastContent": "none",
+            "publishTime": "2016-10-13T11:47:33Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "okMBozdvIx1vKN2yEFLn-BMEGdI",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "NBuf__icGCQ"
+          },
+          "snippet": {
+            "publishedAt": "2016-10-13T12:03:32Z",
+            "channelId": "UCLRlryMfL8ffxzrtqv0_k_w",
+            "title": "ROGUE ONE: A Star Wars Story Trailer 3 (2016)",
+            "description": "Official \"Rogue One: A Star Wars Story\" Movie Trailer 3 2016 | Subscribe ➤ http://abo.yt/kc | Felicity Jones Movie #Trailer | Release: 16 Dec 2016 ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/NBuf__icGCQ/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/NBuf__icGCQ/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "KinoCheck International",
+            "liveBroadcastContent": "none",
+            "publishTime": "2016-10-13T12:03:32Z"
+          }
+        },
+        {
+          "kind": "youtube#searchResult",
+          "etag": "Bi2GVOUzcQ11vFbT7rz0pBnOD3w",
+          "id": {
+            "kind": "youtube#video",
+            "videoId": "WHWkC1Vceko"
+          },
+          "snippet": {
+            "publishedAt": "2016-10-13T14:54:16Z",
+            "channelId": "UCagplPOBQE4pDM75n7h6jPA",
+            "title": "Rogue One: A Star Wars Story Official Trailer #3 (2016) -- Regal Cinemas [HD]",
+            "description": "Rogue One: A Star Wars Story opens December 16, 0216. The Rebellion makes a risky move to steal the plans to the Death Star, setting up the epic saga to ...",
+            "thumbnails": {
+              "_default": null,
+              "medium": {
+                "url": "https://i.ytimg.com/vi/WHWkC1Vceko/mqdefault.jpg",
+                "width": 320,
+                "height": 180
+              },
+              "high": {
+                "url": "https://i.ytimg.com/vi/WHWkC1Vceko/hqdefault.jpg",
+                "width": 480,
+                "height": 360
+              }
+            },
+            "channelTitle": "Regal",
+            "liveBroadcastContent": "none",
+            "publishTime": "2016-10-13T14:54:16Z"
+          }
+        }
+      ]
+    }
+  }
+];
+
+function showData() {
+  var element = document.getElementsByClassName('results-view')[0];
+  for (var i = 0; i < data.length; i++) {
+    var movie = data[i];
+    var movie_div = document.createElement('div');
+    var movie_title = document.createElement('h3'); movie_title.textContent = movie.imdbData.movie.title; movie_div.appendChild(movie_title);
+    var movie_release = document.createElement('p'); movie_release.textContent = movie.imdbData.rating.year; movie_div.appendChild(movie_release);
+    var movie_rating_imdb = document.createElement('p'); movie_rating_imdb.textContent = movie.imdbData.rating.imDb; movie_div.appendChild(movie_rating_imdb);
+    var movie_description = document.createElement('p'); movie_description.textContent = movie.imdbData.rating.description; movie_div.appendChild(movie_description);
+    var movie_thumbnail = document.createElement('img'); movie_thumbnail.src = movie.youtubeTrailersData.items[0].snippet.thumbnails.high.url; movie_div.appendChild(movie_thumbnail);
+
+    element.appendChild(movie_div);
+  }
+}
+
+showData();
